243415c8b7f8d8cb11d2d8e7e2bfffd82b1a3db7
Amirani <amiraniburjanadze@gmail.com>
2023-12-26 21:13:34 +0400

    create starting map

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,31 @@
-import './style.css'
-import { Game, WEBGL } from 'phaser';
-const canvas = document.getElementById('game') as HTMLCanvasElement;
-import { GameScene } from './scenes/playScene';
+import "./style.css";
+import Phaser from "phaser";
+import { Game, WEBGL } from "phaser";
+const canvas = document.getElementById("game") as HTMLCanvasElement;
+import { GameScene } from "./scenes/playScene";
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
   type: WEBGL,
   width: 1608,
   height: 888,
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-},
+  },
   canvas,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-     
       gravity: { y: 0 },
       // debug: true
-    }
+    },
   },
-  scene: [
-    GameScene
-  ]
-}
+  scene: [GameScene],
+};
 const game = new Game(config);
-
 
 // // add resize listener
 // window.addEventListener("resize", () => {
 //   game.scale.resize(window.innerWidth, window.innerHeight);
 
 // });
-
--- src/scenes/playScene.ts
+++ src/scenes/playScene.ts
@@ -1,56 +1,29 @@
 import { Scene, GameObjects } from "phaser";
 
 export class GameScene extends Scene {
-    private textbox: GameObjects.Text | undefined;
-  
-    constructor() {
-      super('scene-game');
-    }
+  private textbox: GameObjects.Text | undefined;
 
-
-    preload() {
-        this.load.image('zombie', '/Dead3.png')
-        this.load.tilemapTiledJSON('map', './map.json');
-        this.load.image('Dungeon_24x24', './P_P_FREE_RPG_TILESET/Dungeon_24x24.png')
-    }
-    
-  
-    create() {
-      console.log(this.make, 'dung')
-
-      const map = this.make.tilemap({ key: 'map' });
-      console.log(map, 'dung')
-      
-      const dungeon = map.addTilesetImage('Dungeon_24x24', 'Dungeon_24x24');
-
-      map.createLayer('Platform', dungeon as any)
-      
-      
-       const zo =  this.add.sprite(200, 200, 'zombie')
-      
-      this.textbox = this.add.text(
-        window.innerWidth / 2,
-        window.innerHeight / 2,
-        'Welcome to Phaser x Vite!',
-        {
-          color: '#FFF',
-          fontFamily: 'monospace',
-          fontSize: '26px'
-        }
-        
-      );
-
-      this.add
-  
-      this.textbox.setOrigin(0.5, 0.5);
-    }
-  
-    update(time: number, delta: number) {
-      if (!this.textbox) {
-        return;
-      }
-  
-      this.textbox.rotation += 0.0005 * delta;
-    }
+  constructor() {
+    super("scene-game");
   }
-  
+
+  preload() {
+    this.load.tilemapTiledJSON("map", "./game.json");
+    this.load.image("dungeon", "./dungeon.png");
+  }
+
+  create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileSet = map.addTilesetImage("dungeon", "dungeon");
+
+    map.createLayer("platform", tileSet as any);
+  }
+
+  update(time: number, delta: number) {
+    if (!this.textbox) {
+      return;
+    }
+
+    this.textbox.rotation += 0.0005 * delta;
+  }
+}
